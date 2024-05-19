8caef1aa3b72265c548ec57abf3f5771b147b901
rolando-d3v <landon3388@gmail.com>
2024-05-19 06:35:09 -0500

    red

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import LogoReact from "./components/react/React";
-import LogoVite from "./components/vite/Vite";
-import css from "./app.module.scss";
+import css from "./app.module.css";
 
 function App() {
-
-  console.log(css);
-
   return (
     <div className={css.app}>
       <div className={css.div_app}>
-        <LogoVite />
         <LogoReact />
       </div>
     </div>
--- src/components/react/React.jsx
+++ src/components/react/React.jsx
@@ -1,30 +1,45 @@
 import React from "react";
+import css from "./react.module.css";
 import reactLogo from "../../assets/react.svg";
-
-import css from "./react.module.scss";
 import { useState } from "react";
 
 export default function LogoReact() {
   const [cambio, setCambio] = useState(false);
   const [cambio2, setCambio2] = useState(false);
 
-  console.log(cambio);
-
   return (
-    <div
-      className={`${css.react}  
-      ${cambio === true ? css.cambio_fondo : css.cambio_fondo_2} 
-      ${cambio2 === false && css.segundo_cambio}
-      `}
-    >
-      <img src={reactLogo} className="logo react" alt="React logo" />
-      <button
-        type="button"
-        className={css.btn_react}
-        onClick={() => setCambio(!cambio)}
-      >
-        Cambio
-      </button>
+    <div className={css.react}>
+      <section className={css.seccion}>
+        <button
+          type="button"
+          className={css.btn_react}
+          // className={css["btn_react"]}
+          onClick={() => setCambio(!cambio)}
+        >
+          boton 01
+        </button>
+        <button
+          type="button"
+          className={`${css.btn_react} ${css.border}`}
+          // className={[css.btn_react, css.border] .join(' ')}
+          onClick={() => setCambio2(!cambio2)}
+        >
+          boton 2
+        </button>
+    
+      </section>
+
+      <figure className={css.content_img}>
+        <img
+          className={`
+           ${cambio === true ? css.borde_azul : css.boder_rojo}
+           ${cambio2 === true && css.cambio_width}
+            `}
+          // className={cambio === true ? css["border_azul"] :  css["border_rojo"]}
+          src={reactLogo}
+          alt="logo"
+        />
+      </figure>
     </div>
   );
 }
