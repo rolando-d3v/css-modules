2e2ef3cddc0bc88ed5242181fc073de41ddd8bdb
rolando-d3v <landon3388@gmail.com>
2024-06-29 07:27:05 -0500

    ree

--- src/components/react/React.jsx
+++ src/components/react/React.jsx
@@ -7,35 +7,36 @@ export default function LogoReact() {
   const [cambio, setCambio] = useState(false);
   const [cambio2, setCambio2] = useState(false);
 
+  console.log(cambio);
+
   return (
     <div className={css.react}>
       <section className={css.seccion}>
         <button
-          type="button"
-          className={css.btn_react}
-          // className={css["btn_react"]}
-          onClick={() => setCambio(!cambio)}
-        >
-          boton 01
+        
+        className={css["btn_react"]}
+        // className={css.btn_react}
+         onClick={() => setCambio(!cambio)}>
+          {" "}
+          boton 01{" "}
         </button>
         <button
-          type="button"
-          className={`${css.btn_react} ${css.border}`}
-          // className={[css.btn_react, css.border] .join(' ')}
+          className={`${css.btn_react} ${css.border} `}
+          // className={[css.btn_react, css.border ] .join(" ") }
           onClick={() => setCambio2(!cambio2)}
         >
+          {" "}
           boton 2
         </button>
-    
       </section>
 
       <figure className={css.content_img}>
         <img
           className={`
-           ${cambio === true ? css.borde_azul : css.boder_rojo}
-           ${cambio2 === true && css.cambio_width}
-            `}
-          // className={cambio === true ? css["border_azul"] :  css["border_rojo"]}
+        ${cambio ? css.border_azul : css.border_rojo}  
+        ${cambio2 && css.cambio_with}
+        `}
+        // className={  cambio  === true  ?  css["border_azul"] :css["border_rojo"]  }
           src={reactLogo}
           alt="logo"
         />
